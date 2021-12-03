35394a65ab5fe3b00e3db6e97539a9eb3a53ab05
Arumugam <jarumugam@outlook.fr>
2021-12-03 17:28:12 +0100

    Piments

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path = require('path')
 
+const saucesRoutes = require('./routes/sauces')
 const userRoutes = require('./routes/user')
 
 mongoose
@@ -32,6 +33,7 @@ app.use(bodyParser.json())
 
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
+app.use('/api/sauces', saucesRoutes)
 app.use('/api/auth', userRoutes)
 
 module.exports = app
